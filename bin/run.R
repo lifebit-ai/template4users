3611b7b19bab72ba610a69c032cd7304ad2e4fe3
hamzah-lifebit <110822354+hamzah-lifebit@users.noreply.github.com>
2024-04-12 09:52:30 +0100

    Add files via upload

--- bin/run.R
+++ bin/run.R
@@ -1,1 +1,16 @@
+#!/usr/bin/env Rscript
 
+# Read the cake types from the input file
+cake_types <- scan("input.csv", what = "character", sep = ",")
+
+# Split the cake types into individual strings
+cake_types <- unlist(strsplit(cake_types, ","))
+
+# Generate the numbers for each cake type
+numbers <- rpois(length(cake_types), 10)
+
+# Combine the cake types and numbers into a data frame
+cake_data <- data.frame(cake_type = cake_types, number = numbers)
+
+# Write the data frame to a CSV file
+write.csv(cake_data, file = "cake_data.csv", row.names = FALSE)
